7f94284385f91fb3c0829d6ebe86988489ecfc42
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2018-04-03 11:38:02 +0200

    Initial commit - IJA project structure and build with test class

---
+++ lib/get-libs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Junit
+rm junit-4.12.jar lib/junit-4.12.jar
+wget http://eva.fit.vutbr.cz/~xfusek08/IJA/junit-4.12.jar
+mv junit-4.12.jar lib/junit-4.12.jar
+
+# hamcrest-core
+rm hamcrest-core-1.3.jar lib/hamcrest-core-1.3.jar
+wget http://eva.fit.vutbr.cz/~xfusek08/IJA/hamcrest-core-1.3.jar
+mv hamcrest-core-1.3.jar lib/hamcrest-core-1.3.jar
