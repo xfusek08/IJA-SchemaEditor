0488bba664e13aada08615ba0deefaafe78fde93
xfrane16 <Jara.Franek2@gmail.com>
2018-04-04 00:33:23 +0200

    Tests Adding base test class. Modifing build.xml tosupport test when run and compile.

---
+++ src/schemaeditor/tests/ModelTests.java
@@ -0,0 +1,39 @@
+package schemaeditor.tests;
+
+import schemaeditor.app.SchemaEditor;
+import java.lang.reflect.Modifier;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Jaromir Franek
+ */
+public class ModelTests
+{
+    
+      
+    @Before
+    public void setUp() 
+    {
+
+    }
+    
+    /**
+     * Zakladni test implementace.
+     */
+    @Test
+    public void test01()
+    {
+        assertEquals(5, 1);
+    }
+
+    /**
+     * Zakladni test implementace.
+     */
+    @Test
+    public void test02()
+    {
+        assertEquals(2, 1);
+    }
+}
