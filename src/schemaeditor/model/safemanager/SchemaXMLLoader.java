53bd95a640a54a2e93bd8423267afeb65a54db2a
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2018-04-04 16:30:06 +0200

    Safemanager balíček - vytvořena kostra s dokumentačními komentáři.

---
+++ src/schemaeditor/model/safemanager/SchemaXMLLoader.java
@@ -0,0 +1,35 @@
+/**
+ * @file:     SchemaXMLLoader.java
+ * @package:  safemanager.model.safemanager
+ * @author    Petr Fusek
+ * @date      04.04.2018
+ */
+package safemanager.model.safemanager;
+
+import schemaeditor.model.safemanager.ISchemaLoader;
+
+/**
+ * Loader saving and loading schemas as XML files.
+ */
+public class SchemaXMLLoader implements ISchemaLoader
+{
+  /**
+   * Loads and returns schema from XML file.
+   *
+   * @param filename Name of xml file where schema is stored
+   */
+  public Schema LoadSchema(string fileName)
+  {
+
+  }
+
+  /**
+   * Saves schema into a XML file.
+   *
+   * @param filename Name of xml file where schema will be stored
+   */
+  public void SaveSchema(Schema schema, string fileName)
+  {
+
+  }
+}
