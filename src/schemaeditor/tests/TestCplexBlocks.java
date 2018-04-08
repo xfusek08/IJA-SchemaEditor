5ca03a5d043831f373703ea8f4355372435a32d4
Petr Fusek <xfusek08@stud.fit.vutbr.cz>
2018-04-08 20:41:24 +0200

    testy pripraveny pro xfusek08

--- src/schemaeditor/tests/TestCplexBlocks.java
+++ src/schemaeditor/tests/TestCplexBlocks.java
@@ -1,34 +1,97 @@
+/**
+ * @file:     TestCplexBlocks.java
+ * @package:  safemanager.tests
+ * @author    Petr Fusek
+ * @date      08.04.2018
+ */
 package schemaeditor.tests;
 
-import schemaeditor.app.SchemaEditor;
 import java.lang.reflect.Modifier;
 import org.junit.Before;
 import org.junit.Test;
+import jdk.nashorn.internal.ir.Block;
 import static org.junit.Assert.*;
 
+import schemaeditor.model.blocks.complex.*;
+
+/** Test class for complex blocks */
 public class TestCplexBlocks
 {
-    @Before
-    public void setUp()
-    {
+  @Before
+  public void setUp()
+  {
+  }
 
-    }
+  @Test
+  public void Test_CplexBlock_Abs()
+  {
+    CplexBlock_Abs block = new CplexBlock_Abs();
+    block.InputPorts.get(0).SetValueByName("real", 3.0);
+    block.InputPorts.get(0).SetValueByName("imaginary", 4.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("number"), 5.0);
+  }
 
-    /**
-     * Zakladni test implementace.
-     */
-    @Test
-    public void test01()
-    {
-        assertEquals(5, 1);
-    }
+  @Test
+  public void Test_CplexBlock_Add()
+  {
+    CplexBlock_Add block = new CplexBlock_Add();
+    block.InputPorts.get(0).SetValueByName("real", 3.0);
+    block.InputPorts.get(0).SetValueByName("imaginary", 4.0);
+    block.InputPorts.get(1).SetValueByName("real", 2.0);
+    block.InputPorts.get(1).SetValueByName("imaginary", -4.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("real"), 5.0);
+    assertEquals(block.OutputPorts.get(0).GetValueByName("imaginary"), 0.0);
+  }
 
-    /**
-     * Zakladni test implementace.
-     */
-    @Test
-    public void test02()
-    {
-        assertEquals(2, 1);
-    }
+  @Test
+  public void Test_CplexBlock_Complement()
+  {
+    CplexBlock_Complement block = new CplexBlock_Complement();
+    block.InputPorts.get(0).SetValueByName("real", 3.0);
+    block.InputPorts.get(0).SetValueByName("imaginary", 4.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("real"), 3.0);
+    assertEquals(block.OutputPorts.get(0).GetValueByName("imaginary"), -4.0);
+  }
+
+  @Test
+  public void Test_CplexBlock_Div()
+  {
+    CplexBlock_Div block = new CplexBlock_Div();
+    block.InputPorts.get(0).SetValueByName("real", 1.0);
+    block.InputPorts.get(0).SetValueByName("imaginary", -3.0);
+    block.InputPorts.get(1).SetValueByName("real", 1.0);
+    block.InputPorts.get(1).SetValueByName("imaginary", 2.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("real"), -1.0);
+    assertEquals(block.OutputPorts.get(0).GetValueByName("imaginary"), -1.0);
+  }
+
+  @Test
+  public void Test_CplexBlock_Mul()
+  {
+    CplexBlock_Mul block = new CplexBlock_Mul();
+    block.InputPorts.get(0).SetValueByName("real", 1.0);
+    block.InputPorts.get(0).SetValueByName("imaginary", 4.0);
+    block.InputPorts.get(1).SetValueByName("real", 5.0);
+    block.InputPorts.get(1).SetValueByName("imaginary", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("real"), 1.0);
+    assertEquals(block.OutputPorts.get(0).GetValueByName("imaginary"), 21.0);
+  }
+
+  @Test
+  public void Test_CplexBlock_Sub()
+  {
+    CplexBlock_Sub block = new CplexBlock_Sub();
+    block.InputPorts.get(0).SetValueByName("real", 1.0);
+    block.InputPorts.get(0).SetValueByName("imaginary", 4.0);
+    block.InputPorts.get(1).SetValueByName("real", 5.0);
+    block.InputPorts.get(1).SetValueByName("imaginary", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("real"), -4.0);
+    assertEquals(block.OutputPorts.get(0).GetValueByName("imaginary"), 3.0);
+  }
 }
--- src/schemaeditor/tests/TestLogicBlocks.java
+++ src/schemaeditor/tests/TestLogicBlocks.java
@@ -1,36 +1,136 @@
+/**
+ * @file:     TestLogicBlocks.java
+ * @package:  safemanager.tests
+ * @author    Petr Fusek
+ * @date      08.04.2018
+ */
 package schemaeditor.tests;
 
-import schemaeditor.app.SchemaEditor;
 import java.lang.reflect.Modifier;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import schemaeditor.model.blocks.logic.*;
+
+/** Test class for complex blocks */
 public class TestLogicBlocks
 {
+  @Before
+  public void setUp()
+  {
+  }
 
+  @Test
+  public void Test_LogicBlock_And()
+  {
+    LogicBlock_And block = new LogicBlock_And();
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
 
-    @Before
-    public void setUp()
-    {
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
 
-    }
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
 
-    /**
-     * Zakladni test implementace.
-     */
-    @Test
-    public void test03()
-    {
-        assertEquals(5, 1);
-    }
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+  }
 
-    /**
-     * Zakladni test implementace.
-     */
-    @Test
-    public void test04()
-    {
-        assertEquals(2, 1);
-    }
+  @Test
+  public void Test_LogicBlock_Or()
+  {
+    LogicBlock_Or block = new LogicBlock_Or();
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+  }
+
+  @Test
+  public void Test_LogicBlock_Xor()
+  {
+    LogicBlock_Xor block = new LogicBlock_Xor();
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
+  }
+
+  @Test
+  public void Test_LogicBlock_Xnor()
+  {
+    LogicBlock_Xor block = new LogicBlock_Xor();
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.InputPorts.get(1).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+  }
+
+  @Test
+  public void Test_LogicBlock_Not()
+  {
+    LogicBlock_Not block = new LogicBlock_Not();
+    block.InputPorts.get(0).SetValueByName("bool", 0.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 1.0);
+
+    block.InputPorts.get(0).SetValueByName("bool", 1.0);
+    block.Calculate();
+    assertEquals(block.OutputPorts.get(0).GetValueByName("bool"), 0.0);
+  }
 }
