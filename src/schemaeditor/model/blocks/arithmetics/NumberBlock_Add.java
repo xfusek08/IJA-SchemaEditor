58f3fce24e138eda65c7b2c20bada93218e01502
xfrane16 <Jara.Franek2@gmail.com>
2018-04-08 17:03:31 +0200

    Zakladni navrh implementace xfrane16
    	-Zakladni implementace trid Port a Block.

--- src/schemaeditor/model/blocks/arithmetics/NumberBlock_Add.java
+++ src/schemaeditor/model/blocks/arithmetics/NumberBlock_Add.java
@@ -1,8 +1,17 @@
 package schemaeditor.model.blocks.arithmetics;
 
 import schemaeditor.model.base.Block;
+import java.util.*;
 
 public class NumberBlock_Add extends Block
 {
+    public NumberBlock_Add(UUID ID, java.lang.String name) 
+    {
+        super(ID, name);
+    }
 
+    public void Calculate()
+    {
+
+    }
 }
